15534067ecf4b559d7b523021f5c1b9a37337b2c
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-09-24 16:33:41 +0330

    create blog and accounts model
    + create Post and Category model in blog app
    + create User and Profile model in accounts app and CustomUserManager

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ['title','author','status','published_date','created_date',]
+    list_filter = ['status','author','published_date',]
+    ordering = ['-published_date',]
+    search_fields = ['title','content',]
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
