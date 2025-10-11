0bcc98058b9f0656fb82874fa72f49e2114b2216
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-10-11 13:16:03 +0330

    created api test by pytest

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -152,7 +152,7 @@ from .filters import PostFilter
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
